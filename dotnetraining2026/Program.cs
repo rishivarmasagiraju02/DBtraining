8b7a485b8f7dcb19b8c136075dc66dd3b762ef82
Rishi Varma <rishivarmasagiraju02@gmail.com>
2026-08-31 16:42:23 -0500

    Added c# assignment

---
+++ dotnetraining2026/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter first number:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second number:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b)
+{
+    Console.WriteLine("Both numbers are equal");
+}
+else
+{
+    Console.WriteLine("Both numbers are not equal");
+}
+
